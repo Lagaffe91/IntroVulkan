7d4a6631ceecee07503f339a51eef7f0231fca2f
Alexandre GAUTHIER <al.gauthier@student.isartdigital.com>
2023-02-17 18:11:21 +0100

    Model rotate

--- APIModernes_Vulkan/src/VKRenderer.cpp
+++ APIModernes_Vulkan/src/VKRenderer.cpp
@@ -1,4 +1,5 @@
 #include <unordered_map>
+#include <chrono>
 #include <set>
 
 #define STB_IMAGE_IMPLEMENTATION
@@ -1455,7 +1456,13 @@ void VKRenderer::UpdateUniformBuffer()
 {
 	UniformBufferObject ubo{};
 
-	ubo.model = glm::rotate(glm::mat4(1.0f), glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+	static auto startTime = std::chrono::high_resolution_clock::now();
+
+	auto currentTime = std::chrono::high_resolution_clock::now();
+	float time = std::chrono::duration<float, std::chrono::seconds::period>(currentTime - startTime).count();
+
+
+	ubo.model = glm::rotate(glm::mat4(1.0f), time * glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
 	ubo.view = glm::lookAt(glm::vec3(-20.0f, -20.0f, -20.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
 	ubo.proj = glm::perspective(glm::radians(45.0f), this->mSwapChain.extent.width / (float)this->mSwapChain.extent.height, 0.1f, 100.0f);
 	ubo.proj[1][1] *= -1;
